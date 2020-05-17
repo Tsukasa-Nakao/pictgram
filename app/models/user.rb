0165826ee7876400fb8ca3cfead24b4493082a49
”Tsukasa <tnakao9909@gmail.com>
2020-05-17 12:58:01 +0000

    user.rdの変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   PW_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :name, presence: true, length:{ maximum: 15 }
+  validates :name, presence: true, length: { maximum: 15 }
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
   validates :password, presence: true, format: { with: PW_REGEX }
   
