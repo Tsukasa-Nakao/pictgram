500a431ffcc16aaf0985048cf75d8a9147ae521d
”Tsukasa <tnakao9909@gmail.com>
2020-05-26 13:12:04 +0000

    sessionsの確認

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
-class SessionController < ApplicationController
+class SessionsController < ApplicationController
   def new
   end
   
   def create
-    user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(email: email_params[:email])
+    if user && user.authenticate(password_params[:password]) 
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
     else
@@ -27,4 +27,13 @@ class SessionController < ApplicationController
     session.delete(:user_id)
     @current_user = nil
   end
+  
+  def email_params
+    params.require(:session).permit(:email)
+  end
+  
+  def password_params
+    params.require(:session).permit(:password)
+  end
 end
+
